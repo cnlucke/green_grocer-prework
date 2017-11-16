193fc81e12a8ff6791327c792385d229a1add957
Cristy Lucke <cristy.lucke@gmail.com>
2017-11-16 20:23:13 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -26,12 +26,17 @@ def apply_coupons(cart, coupons)
         new_cart[item + " W/COUPON"] = {}
         new_cart[item + " W/COUPON"][:price] = coupon_cost
         new_cart[item + " W/COUPON"][:clearance] = item_info[:clearance]
-        puts "item_info[:count]: #{item_info[:count]}"
-        puts "new_cart[item][:count] #{new_cart[item][:count]}!"
-
+        new_cart[item + " W/COUPON"][:count] = item_info[:count] - new_cart[item][:count]
+      elsif item_info[:count] == coupon_num #no regularly-priced items added
+        new_cart[item + " W/COUPON"] = {}
+        new_cart[item + " W/COUPON"][:price] = coupon_cost
+        new_cart[item + " W/COUPON"][:clearance] = item_info[:clearance]
         new_cart[item + " W/COUPON"][:count] = item_info[:count] - new_cart[item][:count]
       else #coupon doesn't apply, add without coupon
-        new_cart[item] = item_info
+        new_cart[item] = {}
+        new_cart[item][:price] = item_info[:price]
+        new_cart[item][:clearance] = item_info[:clearance]
+        new_cart[item][:count] = item_info[:count] % coupon_num
       end
     else #add item without coupon
       new_cart[item] = item_info
